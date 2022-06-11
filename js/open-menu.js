23372063df9c1b478b77f2160d3437d2f5885b22
Dmitriy <rsp.fdrf@yandex.ru>
2022-06-11 20:49:04 +0300

    добавил скрипт для кнопок меню

---
+++ js/open-menu.js
@@ -0,0 +1,10 @@
+const button=document.querySelector(".page-header__toggle--toggle");
+      const opening=document.querySelectorAll(".open");  
+      const closing=document.querySelector(".main-nav_close-button");
+
+      button.addEventListener("click", function() {
+      opening.forEach(x => x.classList.toggle("open"));  
+},{passive: true});
+      closing.addEventListener("click", function() {
+      opening.forEach(x => x.classList.toggle("open"));  
+},{passive: true});
