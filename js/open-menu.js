011f98f034c8de247d25682c7ab4c4d120c1844b
Dmitriy <rsp.fdrf@yandex.ru>
2022-08-01 22:31:08 +0500

    Добавил popup со стилями на отзыв

--- js/open-menu.js
+++ js/open-menu.js
@@ -1,6 +1,6 @@
 const button=document.querySelector(".page-header__toggle--toggle");
-      const opening=document.querySelectorAll(".open");  
-      const closing=document.querySelector(".main-nav_close-button");
+const opening=document.querySelectorAll(".open");  
+const closing=document.querySelector(".main-nav_close-button");
 
       button.addEventListener("click", function() {
       opening.forEach(x => x.classList.toggle("open"));  
