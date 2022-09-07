1139c616eef5c52822878f395a7298d7a38e43f0
Dmitriy <rsp.fdrf@yandex.ru>
2022-09-07 13:45:29 +0500

    Code review CSS, JS

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ likes.forEach (likes => {
     
     if(likes.classList.contains('liked')) {
       likes.classList.remove('liked');
-      for (let likesNumber of likesNumbers) {
+      for (const likesNumber of likesNumbers) {
         likesNumber.textContent--;
       }
         
@@ -30,53 +30,67 @@ likes.forEach (likes => {
     else {
       likesNumbers.textContent++;
         likes.classList.add('liked');
-        for (let likesNumber of likesNumbers) {
+        for (const likesNumber of likesNumbers) {
           likesNumber.textContent++;
         }
     }
   })
 });
 // Открытие/закрытие попапа и создание текста внутри попапа в зависимости от отзыва
-  
+const POPUP_THANK_YOU = "Безумно рады что Вам понравилось, приезжайте к нам еще!"; 
+const WE_ARE_SORRY = "Нам очень жаль, что у Вас сложилось такое впечатление. Мы обязательно учтём все пожелания, чтобы в следующий раз Вам у нас понравилось.";
+const COME_BACK_LATER = "В следующий раз мы обязательно придумаем как Вас порадовать, приезжайте к нам еще!"; 
 
-const popup = document.querySelector(".popup");
-const popupButton = document.querySelector(".form-button");
-const popupClose = document.querySelector(".popup_close"); 
-
-popupButton.addEventListener("click", function() {
-  if (document.getElementById("good").checked) {
-    document.querySelector(".popup_text").innerHTML = 'Безумно рады что Вам понравилось, приезжайте к нам еще!';
-  }
-  else if (document.getElementById("bad").checked) {
-    document.querySelector(".popup_text").innerHTML = 'Нам очень жаль, что у Вас сложилось такое впечатление. Мы обязательно учтём все пожелания, чтобы в следующий раз Вам у нас понравилось.';    
-    }
-  else {
-    document.querySelector(".popup_text").innerHTML = 'В следующий раз мы обязательно придумаем как Вас порадовать, приезжайте к нам еще!'
-  }
-  popup.classList.add("popup-open");
-
-  document.addEventListener("keydown", function (evt) {
-    if (evt.key === ("Escape"||"Esc")) {
-      popup.classList.remove("popup-open")    
-    }
-  }); 
-  document.addEventListener("mousedown", function (e) {
-    if(e.target.closest(".popup_content")=== null) {
-      popup.classList.remove("popup-open")
-    }
-  });
-
-  popupClose.addEventListener("click", function() {
-    popup.classList.remove("popup-open")  
-  });
-});
-
-// Проверка на валидацию формы
+const popupEl = document.querySelector(".popup");
+const popupButtonEl = document.querySelector(".form-button");
+const popupCloseEl = document.querySelector(".popup_close"); 
+const popupTextEl = document.querySelector(".popup_text"); 
 
 const userNameInput = document.querySelector('.js-input-name');
 const userSecondNameInput = document.querySelector('.js-input-secondname');
 const userNumInput = document.querySelector('.js-input-num');
 const userMailInput = document.querySelector('.js-input-mail');
+
+
+popupButtonEl.addEventListener("click", function() {
+
+  if (userNameInput.value.length < 2 || userSecondNameInput.value.length <2 || userNumInput.value.length < 5 || userMailInput.value.length < 6) {
+    alert('Заполните пожалуйста пропущенные поля!')
+  } else {
+    popupButtonEl.removeAttribute('disabled')
+  
+
+  if (document.getElementById("good").checked) {
+    popupTextEl.innerHTML = POPUP_THANK_YOU;
+  }
+  else if (document.getElementById("bad").checked) {
+    popupTextEl.innerHTML = WE_ARE_SORRY;
+  }
+  else {
+    popupTextEl.innerHTML = COME_BACK_LATER;
+  }
+
+    popupEl.classList.add("popup-open");
+  }
+});
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === ("Escape"||"Esc")) {
+    popupEl.classList.remove("popup-open")    
+  }
+}); 
+document.addEventListener("mousedown", function (evt) {
+  if(evt.target.closest(".popup_content")=== null) {
+    popupEl.classList.remove("popup-open")
+  }
+});
+
+popupCloseEl.addEventListener("click", function() {
+  popupEl.classList.remove("popup-open")  
+});
+
+// Проверка на валидацию формы
+
 
 userNameInput.addEventListener('input', () => {
   const valueLength = userNameInput.value.length;
@@ -84,9 +98,9 @@ userNameInput.addEventListener('input', () => {
   const maxLength = 25;
 
   if (valueLength < minLength) {
-    userNameInput.setCustomValidity('Ещё минимум ' + (minLength - valueLength) + ' симв.');
+    userNameInput.setCustomValidity(`Ещё минимум ${minLength - valueLength} симв.`);
   } else if (valueLength > maxLength) {
-    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - maxLength) + ' симв.');
+    userNameInput.setCustomValidity(`Удалите лишние ${valueLength - maxLength} симв.`);
   } else {
     userNameInput.setCustomValidity('');
   }
@@ -100,9 +114,9 @@ userSecondNameInput.addEventListener('input', () => {
   const maxLength = 25;
 
   if (valueLength < minLength) {
-    userSecondNameInput.setCustomValidity('Ещё минимум ' + (minLength - valueLength) + ' симв.');
+    userSecondNameInput.setCustomValidity(`Ещё минимум ${minLength - valueLength} симв.`);
   } else if (valueLength > maxLength) {
-    userSecondNameInput.setCustomValidity('Удалите лишние ' + (valueLength - maxLength) + ' симв.');
+    userSecondNameInput.setCustomValidity(`Удалите лишние ${valueLength - maxLength} симв.`);
   } else {
     userSecondNameInput.setCustomValidity('');
   }
@@ -116,9 +130,9 @@ userNumInput.addEventListener('input', () => {
   const maxLength = 25;
 
   if (valueLength < minLength) {
-    userNumInput.setCustomValidity('Ещё минимум ' + (minLength - valueLength) + ' симв.');
+    userNumInput.setCustomValidity(`Ещё минимум ${minLength - valueLength} симв.`);
   } else if (valueLength > maxLength) {
-    userNumInput.setCustomValidity('Удалите лишние ' + (valueLength - maxLength) + ' симв.');
+    userNumInput.setCustomValidity(`Удалите лишние ${valueLength - maxLength} симв.`);
   } else {
     userNumInput.setCustomValidity('');
   }
@@ -131,7 +145,7 @@ userMailInput.addEventListener('input', () => {
   const minLength = 6;
   
   if (valueLength < minLength) {
-    userMailInput.setCustomValidity('Ещё минимум ' + (minLength - valueLength) + ' симв.');
+    userMailInput.setCustomValidity(`Ещё минимум ${minLength - valueLength} симв.`);
   } else {
     userMailInput.setCustomValidity('');
   }
